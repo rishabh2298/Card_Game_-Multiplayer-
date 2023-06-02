f191b35b58151d22f611b9cd7b220a451a402cac
rishabh2298 <rishabhmishra2298@gmail.com>
2023-06-03 04:29:47 +0530

    Test method for testGameOver created, this will check weather there is any player who had won game or not

--- CardGame/src/test/java/com/swiggy/usecase/GameTest.java
+++ CardGame/src/test/java/com/swiggy/usecase/GameTest.java
@@ -2,6 +2,7 @@ package com.swiggy.usecase;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,7 +15,7 @@ import com.swiggy.service.PlayerService;
 import com.swiggy.service.PlayerServiceImplementation;
 
 public class GameTest {
-	
+
 	private Card currentCard;
 	private Deck deck;
 	private Card[] discardPile;
@@ -37,11 +38,27 @@ public class GameTest {
 		
 		deckService.shuffle(deck.getDeckOfCards());
 		
+		totalPlayers = new Player[2];
+		
+		totalPlayers[0] = new Player("Tanmay", new Card[52]);
+		totalPlayers[1] = new Player("Harish", new Card[52]);
+		
+		for(int player=0; player<totalPlayers.length; player++) {
+			
+			// giving 5-cards initially to eachPlayer
+			
+			for(int card=0; card<5; card++) {
+				totalPlayers[player].getCardsInHand()[totalPlayers[player].getIndexOfNextNewCard()] = deckService.getTopCard(deck.getDeckOfCards());
+				totalPlayers[player].setIndexOfNextNewCard(totalPlayers[player].getIndexOfNextNewCard()+1);
+			}
+		}
 	}
 	
 	
 	@Test
 	public void testDistributeCardsToPlayer() {
+		
+		// this method is private inside of Game class
 		
 		totalPlayers = new Player[2];
 		
@@ -53,7 +70,32 @@ public class GameTest {
 			for(int currentPlayer=0; currentPlayer<totalPlayers.length; currentPlayer++) {
 				playerService.pickCard(totalPlayers[currentPlayer], deckService.getTopCard(deck.getDeckOfCards()));
 				
-				assertTrue(totalPlayers[currentPlayer] != null, "currentPlayer is not getting equal cards, Please check PickCard method of PlayerService interface implementation");
+				Assertions.assertNotNull(totalPlayers[currentPlayer], "currentPlayer is not getting equal cards, Please check PickCard method of PlayerService interface implementation");
+			}
+		}
+	}
+	
+	
+	@Test
+	public void testGameOver() {
+		
+		// this is a private method inside game class
+		// this method will check if any of the palyer has won the game or not
+		
+		for(int player=0; player<totalPlayers.length; player++) {
+			
+			for(int card=0; card<totalPlayers[player].getCardsInHand().length; card++) {
+				
+				// this will execute if 1st and next card of player is null
+				// means he is the winner
+				if(card==0) {
+					Assertions.assertNotNull(totalPlayers[player].getCardsInHand()[card], "This player must have winner, but there is some logic error in cardInHands implementation of player");
+					Assertions.assertNotNull(totalPlayers[player].getCardsInHand()[card+1], "This player must have winner, but there is some logic error in cardInHands implementation of player");
+				}
+				// only not-valid cards available to can break out this inner loop
+				else {
+					break;
+				}
 			}
 		}
 	}
